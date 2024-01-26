ebfa020eee4a260cdf896dacc8b21e88329ccfe7
Erik Hanchett <ehhanche@amazon.com>
2024-01-26 11:55:17 -0800

    error

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { Authenticator } from "@aws-amplify/ui-vue";
+import { Amplify } from "aws-amplify";
+import config from "../amplifyconfiguration.json";
+Amplify.configure(config);
+import "@aws-amplify/ui-vue/styles.css";
+</script>
+
+<template>
+  <header></header>
+
+  <main>
+    <h1>Hello World</h1>
+    <Authenticator>
+      <template #="{ signOut }">
+        <div class="center">
+          <h1>Welcome signed in</h1>
+          <button @click="signOut">Sign Out</button>
+        </div>
+      </template>
+    </Authenticator>
+  </main>
+</template>
+
+<style scoped></style>
